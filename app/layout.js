34ab8f0fd24c447c3b595cc9aeb86855d0ff121d
Kheng Chamnan <chamnankheng18@gmail.com>
2025-11-13 23:54:12 +0700

    My Portfolio

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { Outfit, Ovo } from "next/font/google";
+import "./globals.css";
+
+const outfitSans = Outfit({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+
+const ovoSans = Ovo({
+  subsets: ["latin"], weight: ["400"]
+});
+
+export const metadata = {
+  title: "Portfolio - Chamnan",
+  description: "",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="scroll-smooth">
+      <body
+        className={`${outfitSans.className} ${ovoSans.className} antialiased leading-8 overflow-x-hidden`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
